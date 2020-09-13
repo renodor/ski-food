d973b470946d08a0fb9b062f128e7c2507fcb644
renaud dor <renaud.dor@gmail.com>
2020-09-13 12:16:16 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p 'Destroy all categories'
+Category.destroy_all
+p '----------> done'
+
+p 'Destroy all dishes'
+Dish.destroy_all
+p '----------> done'
+
+
+p 'Create Categories'
+Category.create!(name: 'food')
+Category.create!(name: 'drinks')
+p "----------> #{Category.count} categories created"
+
+p 'Create Dishes'
+dish = Dish.new(name: 'burger', price: 8)
+dish.category = Category.first
+dish.save
+
+dish = Dish.new(name: 'cheese mountain', price: 10)
+dish.category = Category.first
+dish.save
+
+dish = Dish.new(name: 'americain steack', price: 10)
+dish.category = Category.first
+dish.save
+
+Dish.new(name: 'sandwitch chicken', price: 10)
+p "----------> #{Dish.count} dishes created"

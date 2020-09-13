0c8c2f7141e01eca1d03e93742f058fedb52cdb6
renaud dor <renaud.dor@gmail.com>
2020-09-13 10:24:58 -0500

    Add associations and validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_many :products
+  has_many :children, class_name: 'Category', foreign_key: 'parent_id'
+  belongs_to :parent, class_name: 'Category', optional: true
+
+  validates :name, presence: true
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,6 @@
 class Dish < ApplicationRecord
+  belongs_to :category, optional: true
+
+  validates :name, :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
 end

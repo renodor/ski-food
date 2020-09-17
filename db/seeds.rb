8cc13db2d81b17e1e1e7225d11f35e26bb04687b
renaud dor <renaud.dor@gmail.com>
2020-09-17 12:08:05 -0500

    Add description to dish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,17 @@ sandwitches = Category.create!(name: 'sandiwtches')
 
 p "----------> #{Category.count} categories created"
 
+# rubocop:disable Layout/LineLength
+description = 'Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Labore ipsum ducimus natus voluptatibus qui. Ducimus earum sed nesciunt voluptas deserunt soluta blanditiis ab, ipsa sunt consectetur est illo quo, porro!'
+# rubocop:enable Layout/LineLength
+
 p 'Create Dishes'
 
 2.times do |n|
   dish = Dish.new(name: "cheesemountain #{n + 1}", price: 11.5)
   dish.ingredients = ['steak (100gr)', 'raclette', 'rösti', 'salade', 'tomate', 'oignon']
   dish.category = cheesemountain
+  dish.description = description
   file = URI.open('https://res.cloudinary.com/skifood/image/upload/v1600029704/seeds/burger3_l1zfp3.jpg')
   dish.photo.attach(io: file, filename: "#{dish.name}.jpg", content_type: 'image/jpg')
   dish.save!
@@ -30,6 +35,7 @@ p 'Create Dishes'
   dish = Dish.new(name: "chickenmountain #{n + 1}", price: 11.5)
   dish.ingredients = %w[poulet raclette rösti salade tomate oignon]
   dish.category = cheesemountain
+  dish.description = description
   file = URI.open('https://res.cloudinary.com/skifood/image/upload/v1600029704/seeds/burger2_ppnxma.jpg')
   dish.photo.attach(io: file, filename: "#{dish.name}.jpg", content_type: 'image/jpg')
   dish.save!
@@ -39,6 +45,7 @@ end
   dish = Dish.new(name: "burger #{n + 1}", price: 9.5)
   dish.ingredients = ['steak (100gr)', 'salade', 'tomate', 'oignon']
   dish.category = burgers
+  dish.description = description
   file = URI.open('https://res.cloudinary.com/skifood/image/upload/v1600029704/seeds/burger_ltovel.jpg')
   dish.photo.attach(io: file, filename: "#{dish.name}.jpg", content_type: 'image/jpg')
   dish.save!
@@ -48,6 +55,7 @@ end
   dish = Dish.new(name: "americain #{n + 1}", price: 10)
   dish.ingredients = ['steak (100gr)', 'salade', 'tomate', 'oignon', 'frites']
   dish.category = americains
+  dish.description = description
   file = URI.open('https://res.cloudinary.com/skifood/image/upload/v1600029705/seeds/panini2_eclzkp.png')
   dish.photo.attach(io: file, filename: "#{dish.name}.png", content_type: 'image/png')
   dish.save!
@@ -57,6 +65,7 @@ end
   dish = Dish.new(name: "panini #{n + 1}", price: 8.5)
   dish.ingredients = %w[tomate mozza pesto]
   dish.category = paninis
+  dish.description = description
   file = URI.open('https://res.cloudinary.com/skifood/image/upload/v1600029703/seeds/panini_mi7qaq.jpg')
   dish.photo.attach(io: file, filename: "#{dish.name}.jpg", content_type: 'image/jpg')
   dish.save!

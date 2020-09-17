0a90ee09af8ba9fac393c1d083cc69453efa3b87
renaud dor <renaud.dor@gmail.com>
2020-09-16 20:19:23 -0500

    Improve navbar management with partial locals, better css and JS structure

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,17 +1,19 @@
 const navBar = () => {
   const navBar = document.querySelector('.navbar');
-  const menuToggle = document.getElementById('menu-toggle')
   if (navBar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= navBar.offsetHeight) {
-        navBar.classList.add('white');
-      } else {
-        navBar.classList.remove('white');
-      }
-    });
+    if (navBar.classList.contains('navbar-transparent')) {
+      window.addEventListener('scroll', () => {
+        if (window.scrollY >= navBar.offsetHeight) {
+          navBar.classList.remove('navbar-transparent');
+        } else {
+          navBar.classList.add('navbar-transparent');
+        }
+      });
+    }
 
+    const menuToggle = document.getElementById('menu-toggle');
     menuToggle.addEventListener('click', event => {
-      event.currentTarget.classList.toggle('active')
+      event.currentTarget.classList.toggle('active');
     })
   }
 }

48b25edf3706eb97fa22caeed58632adf541ce7d
renaud dor <renaud.dor@gmail.com>
2020-09-17 17:10:00 -0500

    Design navbar collapse

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,13 +1,22 @@
 const navBar = () => {
   const navBar = document.querySelector('.navbar');
+
+  const toggleNavbarTransparent = () => {
+    if (window.scrollY >= navBar.offsetHeight) {
+      navBar.classList.remove('navbar-transparent');
+    } else {
+      navBar.classList.add('navbar-transparent');
+    }
+  };
+
   if (navBar) {
-    if (navBar.classList.contains('navbar-transparent')) {
+    if (navBar.classList.contains('navbar-homepage')) {
+      window.addEventListener('load', () => {
+        toggleNavbarTransparent();
+      });
+
       window.addEventListener('scroll', () => {
-        if (window.scrollY >= navBar.offsetHeight) {
-          navBar.classList.remove('navbar-transparent');
-        } else {
-          navBar.classList.add('navbar-transparent');
-        }
+        toggleNavbarTransparent();
       });
     }
 

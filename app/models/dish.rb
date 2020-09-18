2d2051d3745b27833ac98797ba65a18785ed5f9e
renaud dor <renaud.dor@gmail.com>
2020-09-18 16:40:20 -0500

    Add fallback for empty translations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -5,5 +5,5 @@ class Dish < ApplicationRecord
   validates :name, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 
-  translates :name, :ingredients, :description
+  translates :name, :ingredients, :description, fallbacks_for_empty_translations: true
 end

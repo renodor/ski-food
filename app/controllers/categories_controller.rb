a37b98411a2e46778832050236a956432fdc5aa5
renaud dor <renaud.dor@gmail.com>
2020-09-19 14:46:07 -0500

    Solve N+1 issue on Categories index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @categories = Category.includes(dishes: [photo_attachment: :blob])
+    @categories = Category.includes(dishes: [:translations, { photo_attachment: :blob }])
     render layout: 'home_page'
   end
 end

e4b50814206945292a857d78f5797e25a3700c2c
renaud dor <renaud.dor@gmail.com>
2020-09-19 10:55:05 -0500

    Improve ingredient feature on Dish edit form

--- app/javascript/components/dish_form.js
+++ app/javascript/components/dish_form.js
@@ -1,5 +1,14 @@
 const dishForm = () => {
-  console.log('hehehe');
+  const form = document.querySelector('.dish-form');
+
+  if (form) {
+    const deleteBtns = document.querySelectorAll('.dish-form .ingredient-field label i');
+    deleteBtns.forEach((deleteBtn) => {
+      deleteBtn.addEventListener('click', (event) => {
+        console.log(event.currentTarget.parentNode.parentNode.remove());
+      });
+    });
+  }
 };
 
 export default dishForm;

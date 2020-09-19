5fcd3de666b7353e7f3eaac840458897808fb68e
renaud dor <renaud.dor@gmail.com>
2020-09-19 12:06:51 -0500

    Solve eslint offenses

--- app/javascript/components/dish_form.js
+++ app/javascript/components/dish_form.js
@@ -2,10 +2,33 @@ const dishForm = () => {
   const form = document.querySelector('.dish-form');
 
   if (form) {
-    const deleteBtns = document.querySelectorAll('.dish-form .ingredient-field label i');
-    deleteBtns.forEach((deleteBtn) => {
-      deleteBtn.addEventListener('click', (event) => {
-        console.log(event.currentTarget.parentNode.parentNode.remove());
+    const ingredientsWrapper = form.querySelector('.ingredients-wrapper');
+    const deleteIngredientBtns = ingredientsWrapper.querySelectorAll('.ingredient-field i');
+    const addIngredientBtn = form.querySelector('.add-ingredient');
+
+    deleteIngredientBtns.forEach((deleteIngredientBtn) => {
+      deleteIngredientBtn.addEventListener('click', (event) => {
+        event.currentTarget.parentNode.remove();
+      });
+    });
+
+    addIngredientBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      ingredientsWrapper.insertAdjacentHTML('beforeend', `
+        <div class="form-group ingredient-field">
+          <input type="text"
+                 name="dish[ingredients][]"
+                 id="dish_ingredients_"
+                 value=""
+                 class="form-control"
+                 data-np-checked="1">
+          <i class="fas fa-trash-alt"></i>
+        </div>`
+      );
+
+      const lastIngredientFieldBtn = ingredientsWrapper.querySelector('.ingredient-field:last-child i');
+      lastIngredientFieldBtn.addEventListener('click', (event) => {
+        event.currentTarget.parentNode.remove();
       });
     });
   }

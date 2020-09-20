9b8cca5dba16fd17e54f5c5147c3c6cf6163f45b
renaud dor <renaud.dor@gmail.com>
2020-09-20 10:13:32 -0500

    Start to design language selection feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   scope '(:locale)', locale: /fr|en/ do
+    devise_for :users
     resources :categories, only: :index
 
     resources :dishes

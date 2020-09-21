954246d02fe0261dbe78a7c18a161c49b23cd43b
renaud dor <renaud.dor@gmail.com>
2020-09-21 12:37:14 -0500

    Language Selector

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_locale
+  before_action :build_locales_array
 
   def set_locale
     I18n.locale = params.fetch(:locale, I18n.default_locale).to_sym
   end
 
   def default_url_options
-    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+    { locale: I18n.locale }
+  end
+
+  def build_locales_array
+    @my_locales = %w[fr en]
   end
 end

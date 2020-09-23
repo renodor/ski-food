8988c8a422d02d909b13ca7a4429ccab7ad1e322
renaud dor <renaud.dor@gmail.com>
2020-09-23 15:03:12 -0500

    Add new, create, edit and update to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,49 @@
 class CategoriesController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :index
+  before_action :check_user_authorization, except: :index
 
   def index
     @categories = Category.includes(dishes: [:translations, { photo_attachment: :blob }])
                           .order(order: :asc)
     render layout: 'home_page'
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def check_user_authorization
+    return if current_user.admin
+
+    flash.alert = 'Need to be Admin to access this page'
+    redirect_to root_path
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     devise_for :users
-    resources :categories, only: :index
-
+    resources :categories
     resources :dishes
     root to: 'categories#index'
   end

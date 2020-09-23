f5166d6f0d6c769456fefeef21e56f93a09e6bff
renaud dor <renaud.dor@gmail.com>
2020-09-23 16:20:31 -0500

    Add simpler route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     devise_for :users
+    devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+    end
+
     resources :categories
     resources :dishes
+
     root to: 'categories#index'
   end
 end
